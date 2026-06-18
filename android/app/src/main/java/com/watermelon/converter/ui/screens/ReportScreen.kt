188e9c18a0a0e6db1e2bfbdcf473a79c3c4095e7
So Muzaffari <so.muzaff@gmail.com>
2026-06-18 21:01:44 +0330

    Update ReportScreen.kt

--- android/app/src/main/java/com/watermelon/converter/ui/screens/ReportScreen.kt
+++ android/app/src/main/java/com/watermelon/converter/ui/screens/ReportScreen.kt
@@ -7,7 +7,6 @@ package com.watermelon.converter.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
