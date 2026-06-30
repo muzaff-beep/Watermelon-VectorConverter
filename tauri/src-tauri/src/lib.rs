e546b89381f7b851492de8aecaf7090664afb8e9
So Muzaffari <so.muzaff@gmail.com>
2026-06-30 12:53:30 +0330

    libs

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -7,6 +7,8 @@ pub mod commands;
 /// Shared app builder, used by both main.rs and (potentially) mobile targets.
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             commands::convert_svg,
             commands::render_svg_preview,
